be6bdfb98e8e96833fde09041f40722d486a4c5c
de Masya <kapustin.com@yandex.ru>
2022-06-07 17:31:37 +0300

    Second Commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -29,9 +30,16 @@ namespace _24ayaRabota
             score = 0;
 
         Rect meteorCoordinates,
-             planetCoordinates,
-             rect1, rect2, rect3,
-             rect4, rect5;
+             planetCoordinates;
+
+        List<Rect> rects = new List<Rect>
+        {
+            new Rect(),
+            new Rect(),
+            new Rect(),
+            new Rect(),
+            new Rect()
+        };
 
         Ellipse meteor = new Ellipse()
         {
@@ -40,36 +48,19 @@ namespace _24ayaRabota
             Fill = Brushes.Red
         };
 
+        List<Rectangle> rectangles = new List<Rectangle>
+        {
+            new Rectangle { Fill = Brushes.Brown },
+            new Rectangle { Fill = Brushes.Brown },
+            new Rectangle { Fill = Brushes.Brown },
+            new Rectangle { Fill = Brushes.Brown },
+            new Rectangle { Fill = Brushes.Brown }
+        };
 
         Rectangle planet = new Rectangle()
         {
             Height = 110,
             Width = 815
-        };
-
-        Rectangle rectangle1 = new Rectangle()
-        {
-            Fill = Brushes.Brown
-        };
-
-        Rectangle rectangle2 = new Rectangle()
-        {
-            Fill = Brushes.Brown
-        };
-
-        Rectangle rectangle3 = new Rectangle()
-        {
-            Fill = Brushes.Brown
-        };
-
-        Rectangle rectangle4 = new Rectangle()
-        {
-            Fill = Brushes.Brown
-        };
-
-        Rectangle rectangle5 = new Rectangle()
-        {
-            Fill = Brushes.Brown
         };
 
         private void Window_Initialized(object sender, EventArgs e)
@@ -103,7 +94,7 @@ namespace _24ayaRabota
             }
             else
             {
-                if (meteorCoordinates.IntersectsWith(rect1) || meteorCoordinates.IntersectsWith(rect2) || meteorCoordinates.IntersectsWith(rect3) || meteorCoordinates.IntersectsWith(rect4) || meteorCoordinates.IntersectsWith(rect5))
+                if (meteorCoordinates.IntersectsWith(rects[0]) || meteorCoordinates.IntersectsWith(rects[1]) || meteorCoordinates.IntersectsWith(rects[2]) || meteorCoordinates.IntersectsWith(rects[3]) || meteorCoordinates.IntersectsWith(rects[4]))
                 {
                     meteor.Fill = Brushes.White;
                     score = 0;
@@ -124,7 +115,7 @@ namespace _24ayaRabota
             meteor.Fill = Brushes.Red;
 
             timer.Start();
-
+                        
             try
             {
                 level = Convert.ToInt32(LevelTB.Text);
@@ -140,11 +131,12 @@ namespace _24ayaRabota
 
             canvas1.Children.Remove(meteor);
             canvas1.Children.Remove(planet);
-            canvas1.Children.Remove(rectangle1);
-            canvas1.Children.Remove(rectangle2);
-            canvas1.Children.Remove(rectangle3);
-            canvas1.Children.Remove(rectangle4);
-            canvas1.Children.Remove(rectangle5);
+
+            canvas1.Children.Remove(rectangles[0]);
+            canvas1.Children.Remove(rectangles[1]);
+            canvas1.Children.Remove(rectangles[2]);
+            canvas1.Children.Remove(rectangles[3]);
+            canvas1.Children.Remove(rectangles[4]);
 
             meteorCoordinates = new Rect(400, 20, meteor.Width, meteor.Width);
             planetCoordinates = new Rect(-5, 510, planet.Width, planet.Width);
@@ -161,102 +153,102 @@ namespace _24ayaRabota
             switch (level)
             {
                 case 1: //первый уровень
-                    rectangle1.Height = 28;
-                    rectangle1.Width = 233;
+                    rectangles[0].Height = 28;
+                    rectangles[0].Width = 233;
 
-                    rectangle2.Height = 28;
-                    rectangle2.Width = 466;
+                    rectangles[1].Height = 28;
+                    rectangles[1].Width = 466;
 
-                    Canvas.SetLeft(rectangle1, 0);
-                    Canvas.SetTop(rectangle1, 270);
+                    Canvas.SetLeft(rectangles[0], 0);
+                    Canvas.SetTop(rectangles[0], 270);
 
-                    Canvas.SetLeft(rectangle2, 331);
-                    Canvas.SetTop(rectangle2, 270);
+                    Canvas.SetLeft(rectangles[1], 331);
+                    Canvas.SetTop(rectangles[1], 270);
 
-                    rect1 = new Rect(0, 270, rectangle1.Width, rectangle1.Height);
+                    rects[0] = new Rect(0, 270, rectangles[0].Width, rectangles[0].Height);
 
-                    rect2 = new Rect(331, 270, rectangle2.Width, rectangle2.Height);
+                    rects[1] = new Rect(331, 270, rectangles[1].Width, rectangles[1].Height);
 
-                    canvas1.Children.Add(rectangle1);
-                    canvas1.Children.Add(rectangle2);
+                    canvas1.Children.Add(rectangles[0]);
+                    canvas1.Children.Add(rectangles[1]);
                     break;
 
                 case 2: //второй уровень
-                    rectangle1.Height = 20;
-                    rectangle1.Width = 425;
+                    rectangles[0].Height = 20;
+                    rectangles[0].Width = 425;
 
-                    rectangle2.Height = 20;
-                    rectangle2.Width = 440;
+                    rectangles[1].Height = 20;
+                    rectangles[1].Width = 440;
 
-                    rectangle3.Height = 20;
-                    rectangle3.Width = 415;
+                    rectangles[2].Height = 20;
+                    rectangles[2].Width = 415;
 
-                    Canvas.SetLeft(rectangle1, 0);
-                    Canvas.SetTop(rectangle1, 170);
+                    Canvas.SetLeft(rectangles[0], 0);
+                    Canvas.SetTop(rectangles[0], 170);
 
-                    Canvas.SetLeft(rectangle2, 356);
-                    Canvas.SetTop(rectangle2, 313);
+                    Canvas.SetLeft(rectangles[1], 356);
+                    Canvas.SetTop(rectangles[1], 313);
 
-                    Canvas.SetLeft(rectangle3, 0);
-                    Canvas.SetTop(rectangle3, 465);
+                    Canvas.SetLeft(rectangles[2], 0);
+                    Canvas.SetTop(rectangles[2], 465);
 
-                    rect1 = new Rect(0, 170, rectangle1.Width, rectangle1.Height);
+                    rects[0] = new Rect(0, 170, rectangles[0].Width, rectangles[0].Height);
 
-                    rect2 = new Rect(359, 313, rectangle2.Width, rectangle2.Height);
+                    rects[1] = new Rect(359, 313, rectangles[1].Width, rectangles[1].Height);
 
-                    rect3 = new Rect(0, 465, rectangle3.Width, rectangle3.Height);
+                    rects[2] = new Rect(0, 465, rectangles[2].Width, rectangles[2].Height);
 
-                    canvas1.Children.Add(rectangle1);
-                    canvas1.Children.Add(rectangle2);
-                    canvas1.Children.Add(rectangle3);
+                    canvas1.Children.Add(rectangles[0]);
+                    canvas1.Children.Add(rectangles[1]);
+                    canvas1.Children.Add(rectangles[2]);
                     break;
 
                 case 3: //третий уровень
-                    rectangle1.Height = 20;
-                    rectangle1.Width = 675;
+                    rectangles[0].Height = 20;
+                    rectangles[0].Width = 675;
 
-                    rectangle2.Height = 110;
-                    rectangle2.Width = 225;
+                    rectangles[1].Height = 110;
+                    rectangles[1].Width = 225;
 
-                    rectangle3.Height = 155;
-                    rectangle3.Width = 205;
+                    rectangles[2].Height = 155;
+                    rectangles[2].Width = 205;
 
-                    rectangle4.Height = 155;
-                    rectangle4.Width = 65;
+                    rectangles[3].Height = 155;
+                    rectangles[3].Width = 65;
 
-                    rectangle5.Height = 35;
-                    rectangle5.Width = 175;
+                    rectangles[4].Height = 35;
+                    rectangles[4].Width = 175;
 
-                    Canvas.SetLeft(rectangle1, 0);
-                    Canvas.SetTop(rectangle1, 213);
+                    Canvas.SetLeft(rectangles[0], 0);
+                    Canvas.SetTop(rectangles[0], 213);
 
-                    Canvas.SetLeft(rectangle2, 250);
-                    Canvas.SetTop(rectangle2, 233);
+                    Canvas.SetLeft(rectangles[1], 250);
+                    Canvas.SetTop(rectangles[1], 233);
 
-                    Canvas.SetLeft(rectangle3, 590);
-                    Canvas.SetTop(rectangle3, 345);
+                    Canvas.SetLeft(rectangles[2], 590);
+                    Canvas.SetTop(rectangles[2], 345);
 
-                    Canvas.SetLeft(rectangle4, 250);
-                    Canvas.SetTop(rectangle4, 343);
+                    Canvas.SetLeft(rectangles[3], 250);
+                    Canvas.SetTop(rectangles[3], 343);
 
-                    Canvas.SetLeft(rectangle5, 415);
-                    Canvas.SetTop(rectangle5, 465);
+                    Canvas.SetLeft(rectangles[4], 415);
+                    Canvas.SetTop(rectangles[4], 465);
 
-                    rect1 = new Rect(0, 213, rectangle1.Width, rectangle1.Height);
+                    rects[0] = new Rect(0, 213, rectangles[0].Width, rectangles[0].Height);
 
-                    rect2 = new Rect(250, 235, rectangle2.Width, rectangle2.Height);
+                    rects[1] = new Rect(250, 235, rectangles[1].Width, rectangles[1].Height);
 
-                    rect3 = new Rect(590, 345, rectangle3.Width, rectangle3.Height);
+                    rects[2] = new Rect(590, 345, rectangles[2].Width, rectangles[2].Height);
 
-                    rect4 = new Rect(250, 345, rectangle4.Width, rectangle4.Height);
+                    rects[3] = new Rect(250, 345, rectangles[3].Width, rectangles[3].Height);
 
-                    rect5 = new Rect(415, 465, rectangle5.Width, rectangle5.Height);
+                    rects[4] = new Rect(415, 465, rectangles[4].Width, rectangles[4].Height);
 
-                    canvas1.Children.Add(rectangle1);
-                    canvas1.Children.Add(rectangle2);
-                    canvas1.Children.Add(rectangle3);
-                    canvas1.Children.Add(rectangle4);
-                    canvas1.Children.Add(rectangle5);
+                    canvas1.Children.Add(rectangles[0]);
+                    canvas1.Children.Add(rectangles[1]);
+                    canvas1.Children.Add(rectangles[2]);
+                    canvas1.Children.Add(rectangles[3]);
+                    canvas1.Children.Add(rectangles[4]);
                     break;
             }
         }
@@ -273,13 +265,13 @@ namespace _24ayaRabota
             {
                 meteorCoordinates.X += runSpeed;
                 Canvas.SetLeft(meteor, meteorCoordinates.X);
-            }            
+            }
 
             if (e.Key == Key.Down) //движение вниз
             {
                 meteorCoordinates.Y += runSpeed;
                 Canvas.SetTop(meteor, meteorCoordinates.Y);
-            }            
+            }
 
             if (e.Key == Key.Right && e.Key == Key.Down) //движение вправо-вниз
             {
---
+++ tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rectangle> rect = new List<Rectangle>
+            {
+
+            };        
+
+            recta
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
